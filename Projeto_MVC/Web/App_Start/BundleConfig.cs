83abe7fb66bd82ced62de984c35c05bc2139b070
will <williancosta.amaral@gmail.com>
2017-06-25 13:30:58 -0300

    Add tema;

--- Projeto_MVC/Web/App_Start/BundleConfig.cs
+++ Projeto_MVC/Web/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-b.css",
                       "~/Content/site.css"));
         }
     }
